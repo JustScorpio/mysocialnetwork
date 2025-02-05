42a312178cf85248d40d285301cdb86994449dcb
Kosarev <skorpiello@gmail.com>
2025-02-06 02:24:08 +0300

    query fixes

--- internal/repository/country_repository.go
+++ internal/repository/country_repository.go
@@ -23,7 +23,8 @@ func (r *CountryRepository) GetAll() ([]models.Country, error) {
 	var countries []models.Country
 	for rows.Next() {
 		var country models.Country
-		if err := rows.Scan(country.Id, country.Name, country.Code); err != nil {
+		err := rows.Scan(&country.Id, &country.Name, &country.Code)
+		if err != nil {
 			return nil, err
 		}
 		countries = append(countries, country)
@@ -34,7 +35,7 @@ func (r *CountryRepository) GetAll() ([]models.Country, error) {
 
 func (r *CountryRepository) Get(id int) (*models.Country, error) {
 	var country models.Country
-	err := r.db.QueryRow("SELECT id, name, code FROM countries WHERE id = ?", id).Scan(&country.Id, &country.Name, &country.Code)
+	err := r.db.QueryRow("SELECT id, name, code FROM countries WHERE id = $1", id).Scan(&country.Id, &country.Name, &country.Code)
 	if err != nil {
 		return nil, err
 	}
@@ -42,26 +43,20 @@ func (r *CountryRepository) Get(id int) (*models.Country, error) {
 }
 
 func (r *CountryRepository) Create(country *models.Country) error {
-	result, err := r.db.Exec("INSERT INTO countries (name, code) VALUES (?, ?)", country.Name, country.Code)
+	err := r.db.QueryRow("INSERT INTO countries (name, code) VALUES ($1, $2) RETURNING id", country.Name, country.Code).Scan(&country.Id)
 	if err != nil {
 		return err
 	}
 
-	id, err := result.LastInsertId()
-	if err != nil {
-		return err
-	}
-
-	country.Id = int(id)
 	return nil
 }
 
 func (r *CountryRepository) Update(country *models.Country) error {
-	_, err := r.db.Exec("UPDATE countries SET name = ?, code = ? WHERE id = ?", country.Name, country.Code, country.Id)
+	_, err := r.db.Exec("UPDATE countries SET name = $1, code = $2 WHERE id = $3", country.Name, country.Code, country.Id)
 	return err
 }
 
 func (r *CountryRepository) Delete(id int) error {
-	_, err := r.db.Exec("DELETE FROM countries WHERE id = ?", id)
+	_, err := r.db.Exec("DELETE FROM countries WHERE id = $1", id)
 	return err
 }
